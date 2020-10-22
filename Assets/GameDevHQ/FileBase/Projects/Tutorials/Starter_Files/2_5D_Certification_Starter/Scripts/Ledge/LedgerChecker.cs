276e90dd5db502b68c2aecf3f79329cd9bc7f8db
GrivaJG <Griva.yg@gmail.com>
2020-10-22 18:52:08 +0300

    working with animation Player

---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification_Starter/Scripts/Ledge/LedgerChecker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LedgerChecker : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 _handPose;
+
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == "Ledge")
+        {
+            Debug.Log("in Ledge_Checker");
+            var player = other.transform.parent.GetComponent<Player>();
+
+            if (player != null)
+                player.GrabLedge(_handPose);
+        }
+    }
+}
--- Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification_Starter/Scripts/Player/Player.cs
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification_Starter/Scripts/Player/Player.cs
@@ -10,51 +10,95 @@ public class Player : MonoBehaviour
 
     [SerializeField] private float _jump = 20f;
 
+    [SerializeField] private Animator _animator;
+    
     private bool _isGround;
 
     private CharacterController _controller;
 
     private Vector3 _velocity;
+    private Vector3 _direction;
 
     private bool _doubleJump;
+    private bool _jumping;
+
+    
 
     private void Start()
     {
         _controller = GetComponent<CharacterController>();
+
+        _animator = GetComponentInChildren<Animator>();
     }
 
     private void Update()
     {
+        CalculateMovement();
+       
+    }
+
+    private void CalculateMovement()
+    {
         _isGround = _controller.isGrounded;
 
-        Debug.Log(_isGround);
-        
         if (_isGround && _velocity.y < 0)
         {
             _velocity.y = 0f;
         }
 
-        Vector3 move = new Vector3(0, 0, Input.GetAxis("Horizontal"));
-        _controller.Move(move * _speed * Time.deltaTime);
+        _direction = new Vector3(0, 0, Input.GetAxisRaw("Horizontal"));
+        _controller.Move(_direction * _speed * Time.deltaTime);
+        _animator.SetFloat("isRunning", Mathf.Abs(Input.GetAxisRaw("Horizontal")));
 
-        
-
-        if (Input.GetButtonDown("Jump") && _isGround)
-        {            
-            _velocity.y += _jump;
+        if (Input.GetAxisRaw("Horizontal") != 0)
+        {
+            if (_animator.GetBool("isGrabLedge") == false)
+            {
+                Vector3 facing = transform.localEulerAngles;
+                facing.y = _direction.z > 0 ? 0 : 180;
+                transform.localEulerAngles = facing;
+            }
+          
         }
-        else if (Input.GetButtonDown("Jump") && _isGround == false && _doubleJump)
+
+        if (_isGround)
+        {
+            _doubleJump = true;
+            _animator.SetBool("isJumping", false);
+
+
+            Debug.Log(_animator.GetBool("isJumping"));
+        }
+
+        if (Input.GetButtonDown("Jump") && _isGround && _animator.GetBool("isGrabLedge") == false)
+        {
+            _velocity.y += _jump;
+            _animator.SetBool("isJumping", true);
+            Debug.Log(_animator.GetBool("isJumping"));
+        }
+        else if (Input.GetButtonDown("Jump") && _isGround == false && _doubleJump && _animator.GetBool("isGrabLedge") == false)
         {
             Debug.Log("double jump");
             _velocity.y += Mathf.Sqrt(_jump * -0.2f * _gravity);
             _doubleJump = false;
+            _animator.SetBool("isJumping", true);
+            Debug.Log(_animator.GetBool("isJumping"));
+        }
+        else if (Input.GetButtonDown("Jump") && _isGround == false && _animator.GetBool("isGrabLedge") == true)
+        {
+
         }
 
-        if (_isGround)
-            _doubleJump = true;
 
         _velocity.y += _gravity * Time.deltaTime;
         _controller.Move(_velocity * Time.deltaTime);
+    }
+
+    public void GrabLedge(Vector3 handPose)
+    {
+        _controller.enabled = false;
+        _animator.SetBool("isGrabLedge", true);
+        transform.position = handPose;
     }
 
 }
