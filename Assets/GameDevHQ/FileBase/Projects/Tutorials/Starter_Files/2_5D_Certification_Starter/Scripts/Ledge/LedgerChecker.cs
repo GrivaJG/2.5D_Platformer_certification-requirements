597e83dd8fc4ec06858e909e3e2f8a9aea399813
GrivaJG <Griva.yg@gmail.com>
2020-10-28 13:38:17 +0300

    Feature: Create Moving Platform; Status: Done

--- Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification_Starter/Scripts/Ledge/LedgerChecker.cs
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification_Starter/Scripts/Ledge/LedgerChecker.cs
@@ -17,6 +17,16 @@ public class LedgerChecker : MonoBehaviour
             Debug.Log("in Ledge_Checker");
             var player = other.transform.parent.GetComponent<Player>();
             _handPose = other.transform.parent.GetComponent<Transform>().position;
+            var modelRotation = other.transform.parent.transform.Find("Model");
+            if (modelRotation == null)
+                Debug.Log("modelRotation is NULL");
+
+            if (modelRotation.transform.localEulerAngles.y == 0)
+                _handPose.z += 0.6f;
+            else
+                _handPose.z -= 0.6f;
+            other.transform.parent.GetComponent<Transform>().position = _handPose;
+            Debug.Log("_handPose: " + _handPose.x + " " + _handPose.y + " " + _handPose.z);
 
             if (player != null)
                 player.GrabLedge(_handPose);
--- Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification_Starter/Scripts/MovingPlatform/MovingPlatform.cs
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification_Starter/Scripts/MovingPlatform/MovingPlatform.cs
@@ -4,6 +4,45 @@ using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour
 {
+    [SerializeField]
+    private Transform _pointA;
+
+    [SerializeField]
+    private Transform _pointB;
+
+    [SerializeField]
+    private float _speed = 4;
+
+    private enum direction
+    {
+        up,
+        down
+    }
+
+    private direction _direct;
+
+    private Vector3 _move;
+
+    private void Start()
+    {
+        _direct = direction.up;       
+    }
+
+
+    private void FixedUpdate()
+    {
+        if (this.transform.position == _pointA.position) // && _direct == direction.up)
+        {
+            _move = _pointB.position;            
+        }
+        else if (this.transform.position == _pointB.position) // && _direct == direction.down)
+        {
+            _move = _pointA.position;
+        }
+
+        this.transform.position = Vector3.MoveTowards(this.transform.position, _move, Time.deltaTime * _speed);
+
+    }
     private void OnTriggerEnter(Collider Player)
     {
         if (Player.transform.tag == "Player")
